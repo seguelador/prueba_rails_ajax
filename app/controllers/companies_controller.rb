b32751f70d33789c77ff17dba03532a579c36f4f
seguelador <matias.seguel.estay@gmail.com>
2016-08-29 20:41:35 -0300

    CRUD companies working, Everything through Ajax, except Show, it was now requested

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,91 @@
-class CompaniesController < InheritedResources::Base
+class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update, :destroy]
+
+  # GET /companies
+  # GET /companies.json
+  def index
+    @companies = Company.all
+  end
+
+  # GET /companies/1
+  # GET /companies/1.json
+  def show
+  end
+
+  # GET /companies/new
+  def new
+    @company = Company.new
+    respond_to do |format|
+    	format.js
+    end
+  end
+
+  # GET /companies/1/edit
+  def edit
+  	respond_to do |format|
+    	format.js
+    end
+  end
+
+  # POST /companies
+  # POST /companies.json
+  def create
+    @company = Company.new(company_params)
+
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.json { render :show, status: :created, location: @company }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /companies/1
+  # PATCH/PUT /companies/1.json
+  def update
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.json { render :show, status: :ok, location: @company }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /companies/1
+  # DELETE /companies/1.json
+  def destroy
+    @company.destroy
+    respond_to do |format|
+      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
+  def list_companies 
+  	@companies = Company.all
+
+  	respond_to do |format|
+      format.js
+    end
+  end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_company
+      @company = Company.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
       params.require(:company).permit(:name)
     end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :companies
+  resources :companies do 
+    collection do 
+      post 'list_companies'
+    end
+  end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
